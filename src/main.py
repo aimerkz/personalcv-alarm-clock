2d9aa69ef684d823c9ce44b1efcc5afe7574e43f
aimerkulov <merk.aimerk@gmail.com>
2025-08-09 17:45:58 +0300

    Refactored, added mypy job

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from core.providers.webdriver import WebDriverProvider
 from services.interfaces import BaseAlarmService
 
 
-def main() -> int:
+def main() -> None:
     container = make_container(
         WebDriverProvider(),
         AlarmProvider(),
